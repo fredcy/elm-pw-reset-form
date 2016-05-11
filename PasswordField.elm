97b4b112cb431ee2d85f8f65db7d45977109984a
Fred Yankowski <fcy@imsa.edu>
2016-05-11 12:32:34 -0500

    Run through elm-format.

--- PasswordField.elm
+++ PasswordField.elm
@@ -4,54 +4,76 @@ import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 
+
 type alias Model =
-  { password : String
-  , showclear : Bool
-  }
+    { password : String
+    , showclear : Bool
+    }
+
 
 init : Model
-init = Model "" False
+init =
+    Model "" False
 
 
-type Msg =
-            SetPassword String
-            | SetShowclear Bool
+type Msg
+    = SetPassword String
+    | SetShowclear Bool
+
 
 
 -- The update function receives `focusCmd`, a function that creates a command
 -- for focusing on this particular form element.
-update : Cmd Msg -> Msg -> Model -> (Model, Cmd Msg)
-update focusCmd msg model =
-  case msg of
-    SetPassword pw -> ( { model | password = pw }, Cmd.none)
-    SetShowclear b -> ( { model | showclear = b }, focusCmd)
-                      
 
--- We separate the static properties from those in the model.                      
+
+update : Cmd Msg -> Msg -> Model -> ( Model, Cmd Msg )
+update focusCmd msg model =
+    case msg of
+        SetPassword pw ->
+            ( { model | password = pw }, Cmd.none )
+
+        SetShowclear b ->
+            ( { model | showclear = b }, focusCmd )
+
+
+
+-- We separate the static properties from those in the model.
+
+
 type alias Props =
-  { name : String
-  , label : String
-  }
+    { name : String
+    , label : String
+    }
+
 
 view : Props -> Model -> Html Msg
 view props model =
-  let
-    showId = props.name ++ "-show"
-  in
-    div []
-          [ label [ for props.name ] [ text props.label ]
-          , input [ type' (if model.showclear then "text" else "password")
-                  , value model.password
-                  , name props.name
-                  , onInput SetPassword
-             ]
-            []
-          , label [ class "show", for showId ] [ text "show" ]
-          , input [ type' "checkbox"
-                  , id showId
-                  , checked model.showclear
-                  , onCheck SetShowclear
-                  , tabindex -1   -- not reached via Tab key
-                  ]
-          []
-        ]  
+    let
+        showId =
+            props.name ++ "-show"
+    in
+        div []
+            [ label [ for props.name ] [ text props.label ]
+            , input
+                [ type'
+                    (if model.showclear then
+                        "text"
+                     else
+                        "password"
+                    )
+                , value model.password
+                , name props.name
+                , onInput SetPassword
+                ]
+                []
+            , label [ class "show", for showId ] [ text "show" ]
+            , input
+                [ type' "checkbox"
+                , id showId
+                , checked model.showclear
+                , onCheck SetShowclear
+                , tabindex -1
+                  -- not reached via Tab key
+                ]
+                []
+            ]
--- Reset.elm
+++ Reset.elm
@@ -7,189 +7,274 @@ import Html.Events exposing (..)
 import Result exposing (andThen)
 import String
 import Task
-
 import PasswordField
 
 
 -- Get static information about form, scraped from the legacy form.
-type alias FormInfo = { action : String
-                      , formkey : String
-                      , formname : String
-                      }
+
+
+type alias FormInfo =
+    { action : String
+    , formkey : String
+    , formname : String
+    }
 
 
 main =
-  Html.programWithFlags { init = init, view = view, update = update, subscriptions = subscriptions }
+    Html.programWithFlags { init = init, view = view, update = update, subscriptions = subscriptions }
+
 
 
 -- Send out password values for evaluation.
+
+
 port pwChanges : String -> Cmd msg
 
+
+
 -- Get back evaluations of password strength.
+
+
 port pwStrength : (Int -> msg) -> Sub msg
 
 
+
 -- Send direction to focus on element.
+
+
 port focus : String -> Cmd msg
-             
+
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-  pwStrength UpdateStrength
-             
+    pwStrength UpdateStrength
+
 
 type alias Model =
-  { password : PasswordField.Model
-  , password2 : PasswordField.Model
-  , confirmed : Bool
-  , strength : Int
-  , formInfo : FormInfo
-  }
+    { password : PasswordField.Model
+    , password2 : PasswordField.Model
+    , confirmed : Bool
+    , strength : Int
+    , formInfo : FormInfo
+    }
 
 
-init : FormInfo -> (Model, Cmd Msg)
+init : FormInfo -> ( Model, Cmd Msg )
 init flags =
-  ( Model PasswordField.init PasswordField.init False 0 flags
-  , sendFocus "#elmResetForm input[name=password]" )
+    ( Model PasswordField.init PasswordField.init False 0 flags
+    , sendFocus "#elmResetForm input[name=password]"
+    )
 
 
-type Msg =
-  UpdatePassword PasswordField.Msg
-  | UpdatePassword2 PasswordField.Msg
-  | UpdateConfirmed Bool
-  | UpdateStrength Int
+type Msg
+    = UpdatePassword PasswordField.Msg
+    | UpdatePassword2 PasswordField.Msg
+    | UpdateConfirmed Bool
+    | UpdateStrength Int
+
 
 
 -- Create a command that sends the password value over the port for evaluation.
+
+
 sendPwChange : String -> Cmd Msg
 sendPwChange pw =
-  pwChanges pw
+    pwChanges pw
+
+
 
 -- Create a command that sends a javascript element-selector to the port for
 -- requesting focus.
-sendFocus: String -> Cmd a
+
+
+sendFocus : String -> Cmd a
 sendFocus selector =
-  focus selector
+    focus selector
 
 
-update : Msg -> Model -> (Model, Cmd Msg)
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-  case msg of
-    UpdatePassword pfMsg ->
-      let focusCmd = sendFocus "#elmResetForm input[name=password]"
-          (password', fx) = PasswordField.update focusCmd pfMsg model.password
-          model' = { model | password = password' }
-          pwCmd = sendPwChange model'.password.password
-      in (model', Cmd.batch [Cmd.map UpdatePassword fx, pwCmd])
-    UpdatePassword2 pfMsg ->
-      let focusCmd = sendFocus "#elmResetForm input[name=password2]"
-          (password2', fx) = PasswordField.update focusCmd pfMsg model.password2
-          model' = { model | password2 = password2' }
-      in (model', Cmd.map UpdatePassword2 fx)
-    UpdateConfirmed b ->
-      ({ model | confirmed = b }, Cmd.none)
-    UpdateStrength i ->
-      ({ model | strength = i }, Cmd.none)
+    case msg of
+        UpdatePassword pfMsg ->
+            let
+                focusCmd =
+                    sendFocus "#elmResetForm input[name=password]"
+
+                ( password', fx ) =
+                    PasswordField.update focusCmd pfMsg model.password
+
+                model' =
+                    { model | password = password' }
+
+                pwCmd =
+                    sendPwChange model'.password.password
+            in
+                ( model', Cmd.batch [ Cmd.map UpdatePassword fx, pwCmd ] )
+
+        UpdatePassword2 pfMsg ->
+            let
+                focusCmd =
+                    sendFocus "#elmResetForm input[name=password2]"
+
+                ( password2', fx ) =
+                    PasswordField.update focusCmd pfMsg model.password2
+
+                model' =
+                    { model | password2 = password2' }
+            in
+                ( model', Cmd.map UpdatePassword2 fx )
+
+        UpdateConfirmed b ->
+            ( { model | confirmed = b }, Cmd.none )
+
+        UpdateStrength i ->
+            ( { model | strength = i }, Cmd.none )
 
 
 view : Model -> Html Msg
 view model =
-  div []
-      [ inputForm model
-      --, debugDisplay model
-      ]
+    div []
+        [ inputForm model
+          --, debugDisplay model
+        ]
+
 
 inputForm : Model -> Html Msg
-inputForm  model =
-  let
-    message = case validPassword model.password.password of
-                Err msg -> msg
-                Ok _ -> ""
-    message2 = if model.password2.password == "" then
-                 ""
-               else if model.password2.password /= model.password.password then
-                 "Does not match"
-               else
-                 ""
-    props = { name = "password", label = "New password" }
-    props2 = { name = "password2", label = "New password, again" }
-  in
-    Html.form [ action model.formInfo.action, method "post", id "elmResetForm" ]
-          [ ol []
-                 [ li [] [ PasswordField.view props model.password |> Html.map UpdatePassword
-                         , if message /= "" then
-                             div [ class "message" ] [ text message ]
-                           else
-                             strengthDisplay model.strength
-                         ]
-                 , li [] [ PasswordField.view props2 model.password2 |> Html.map UpdatePassword2
-                         , div [ class "message" ] [ text message2 ]
-                         ]
-                 , li []
-                        [ label [ for "accept" ] [ text "Accept ITS Policy and Honor Code" ]
-                        , input [ type' "checkbox"
-                                , id "accept"
-                                , name "accept"
-                                , checked model.confirmed
-                                , onCheck UpdateConfirmed
-                                ] []
+inputForm model =
+    let
+        message =
+            case validPassword model.password.password of
+                Err msg ->
+                    msg
+
+                Ok _ ->
+                    ""
+
+        message2 =
+            if model.password2.password == "" then
+                ""
+            else if model.password2.password /= model.password.password then
+                "Does not match"
+            else
+                ""
+
+        props =
+            { name = "password", label = "New password" }
+
+        props2 =
+            { name = "password2", label = "New password, again" }
+    in
+        Html.form [ action model.formInfo.action, method "post", id "elmResetForm" ]
+            [ ol []
+                [ li []
+                    [ PasswordField.view props model.password |> Html.map UpdatePassword
+                    , if message /= "" then
+                        div [ class "message" ] [ text message ]
+                      else
+                        strengthDisplay model.strength
+                    ]
+                , li []
+                    [ PasswordField.view props2 model.password2 |> Html.map UpdatePassword2
+                    , div [ class "message" ] [ text message2 ]
+                    ]
+                , li []
+                    [ label [ for "accept" ] [ text "Accept ITS Policy and Honor Code" ]
+                    , input
+                        [ type' "checkbox"
+                        , id "accept"
+                        , name "accept"
+                        , checked model.confirmed
+                        , onCheck UpdateConfirmed
                         ]
-                 ]
-          , if ready model then submitButton model else div [] []
-          , input [ type' "hidden", name "_formname", value model.formInfo.formname ] []
-          , input [ type' "hidden", name "_formkey", value model.formInfo.formkey ] []
-          ]
-      
+                        []
+                    ]
+                ]
+            , if ready model then
+                submitButton model
+              else
+                div [] []
+            , input [ type' "hidden", name "_formname", value model.formInfo.formname ] []
+            , input [ type' "hidden", name "_formkey", value model.formInfo.formkey ] []
+            ]
+
+
 strengthDisplay : Int -> Html Msg
 strengthDisplay strength =
-  let
-    score = strength
-    (message, class') =
-      case score of
-        0 -> ("weak: too guessable", "score-0")
-        1 -> ("weak: very guessable", "score-1")
-        2 -> ("weak: somewhat guessable", "score-2" )
-        3 -> ("good: safely unguessable", "score-3")
-        4 -> ("good: very unguessable", "score-4")
-        _ -> ("unknown", "score-error")
-  in
-    div [ class ("score-bar " ++ class') ]
-          [ text message ]
+    let
+        score =
+            strength
+
+        ( message, class' ) =
+            case score of
+                0 ->
+                    ( "weak: too guessable", "score-0" )
+
+                1 ->
+                    ( "weak: very guessable", "score-1" )
+
+                2 ->
+                    ( "weak: somewhat guessable", "score-2" )
+
+                3 ->
+                    ( "good: safely unguessable", "score-3" )
+
+                4 ->
+                    ( "good: very unguessable", "score-4" )
+
+                _ ->
+                    ( "unknown", "score-error" )
+    in
+        div [ class ("score-bar " ++ class') ]
+            [ text message ]
+
 
 debugDisplay : Model -> Html Msg
 debugDisplay model =
-  div []
+    div []
         [ h3 [] [ text "Model" ]
         , toString model |> text
         , h3 [] [ text "Strength" ]
         , toString model.strength |> text
         ]
 
+
 submitButton : Model -> Html Msg
 submitButton model =
-  if ready model then
-    input [ type' "submit" ] []
-  else
-    text ""
+    if ready model then
+        input [ type' "submit" ] []
+    else
+        text ""
+
 
 
 -- Evaluate the proposed password against the absolute local requirements.
+
+
 validPassword : String -> Result String ()
 validPassword str =
-  if String.contains "'" str then
-    Err "Password should not include the single-quote character (')"
-  else if String.contains "\"" str then
-         Err "Password should not include the double-quote character (\")"
-       else if String.length str < 8 then
-              Err "Password must be at least 8 characters long"
-            else 
-              Ok ()
+    if String.contains "'" str then
+        Err "Password should not include the single-quote character (')"
+    else if String.contains "\"" str then
+        Err "Password should not include the double-quote character (\")"
+    else if String.length str < 8 then
+        Err "Password must be at least 8 characters long"
+    else
+        Ok ()
+
+
 
 -- Is the data ready for submitting back to the server?
+
+
 ready : Model -> Bool
 ready model =
-  let valid = case validPassword model.password.password of
-                Err _ -> False
-                Ok _ -> True
-  in valid && model.password.password == model.password2.password && model.confirmed
+    let
+        valid =
+            case validPassword model.password.password of
+                Err _ ->
+                    False
 
+                Ok _ ->
+                    True
+    in
+        valid && model.password.password == model.password2.password && model.confirmed
